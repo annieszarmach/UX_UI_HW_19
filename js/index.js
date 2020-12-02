349a493c9c81c806757ec8348940fe1a19a9ee54
annieszarmach <67974426+annieszarmach@users.noreply.github.com>
2020-12-02 09:59:45 -0600

    hover

---
+++ js/index.js
@@ -0,0 +1,9 @@
+console.log("index file loaded");
+
+
+$(".button").hover(function(){
+    $(this).css("background-color","#B56B22");
+    }, function(){
+    $(this).css("background-color","#9F4D20");
+
+});
