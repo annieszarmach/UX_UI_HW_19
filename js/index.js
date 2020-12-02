0d38bfbaea1fcd61a350ca51ecd0764edef57582
annieszarmach <67974426+annieszarmach@users.noreply.github.com>
2020-12-02 15:03:37 -0600

    bold text mouse enter

--- js/index.js
+++ js/index.js
@@ -7,3 +7,31 @@ $(".button").hover(function(){
     $(this).css("background-color","#9F4D20");
 
 });
+
+$(".mywork")
+.on( "mouseenter", function() {
+$( this ).css({
+"border-bottom": "#ADB7B9",
+"font-weight": "bold",
+});
+})
+.on( "mouseleave", function() {
+var styles = {
+borderBottom : "none",
+fontWeight: ""
+};
+$( this ).css( styles );
+});
+
+$(".about")
+.on( "mouseenter", function() {
+$( this ).css({
+"font-weight": "bold",
+});
+})
+.on( "mouseleave", function() {
+var styles = {
+fontWeight: ""
+};
+$( this ).css( styles );
+});
